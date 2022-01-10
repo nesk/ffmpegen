ceb96a3519d2da1a4773624e56c6cbd6cb355903
Johann Pardanaud <johann@pardanaud.com>
2022-01-10 20:58:40 +0100

    merge videoprogress.tsx into videotimeline.tsx

--- src/Video/Controls/VideoTimeline.tsx
+++ src/Video/Controls/VideoTimeline.tsx
@@ -1,6 +1,36 @@
 import { useRef, useState, useEffect, FC } from "react"
+import styled from "styled-components"
 import { useDragDropTracking } from "../../MouseMoveEvents"
-import { InnerVideoProgress, VideoProgress, VideoProgressProps } from "./VideoProgress"
+
+export interface VideoProgressProps {
+  readonly currentTime: number
+  readonly duration: number
+}
+
+export const VideoProgress = styled.div`
+  width: 100%;
+  height: 25px;
+  overflow: hidden;
+  user-select: none;
+`
+
+export const InnerVideoProgress = styled.div.attrs<VideoProgressProps>(({ currentTime, duration }) => ({
+  style: { width: `${(currentTime / duration) * 100}%` },
+}))<VideoProgressProps>`
+  position: relative;
+  width: 0;
+  height: 100%;
+
+  &::after {
+    position: absolute;
+    top: 0;
+    right: -2px;
+    width: 4px;
+    height: 100%;
+    background: #fff;
+    content: "";
+  }
+`
 
 /**
  * Computes the current time based on the position of the cursor.
