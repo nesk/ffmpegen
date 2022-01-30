2b02613c19096feacef7e8fcb44bd4949f899984
Johann Pardanaud <johann@pardanaud.com>
2022-01-30 18:50:55 +0100

    create a helper file for common time methods

--- src/CliCode.tsx
+++ src/CliCode.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react"
 import styled from "styled-components"
+import { formatSecondsToFfmpegTime } from "./helpers"
 
 const StyledCode = styled.code`
   display: block;
@@ -23,22 +24,4 @@ export const CliCode: FC<CliCodeProps> = ({ file, startTime, endTime }) => {
       copy {output}
     </StyledCode>
   )
-}
-
-const formatSecondsToFfmpegTime = (seconds: number) => {
-  const h = Math.floor(seconds / 3600)
-    .toString()
-    .padStart(2, "0")
-  seconds = seconds % 3600
-
-  const m = Math.floor(seconds / 60)
-    .toString()
-    .padStart(2, "0")
-  seconds = seconds % 60
-
-  const s = Math.round(seconds).toString().padStart(2, "0")
-
-  const ms = Math.round((seconds - Math.floor(seconds)) * 1000)
-
-  return `${h}:${m}:${s}.${ms}`
 }
---
+++ src/helpers.ts
@@ -0,0 +1,19 @@
+export const formatSecondsToFfmpegTime = (seconds: number) => {
+  const h = Math.floor(seconds / 3600)
+    .toString()
+    .padStart(2, "0")
+  seconds = seconds % 3600
+
+  const m = Math.floor(seconds / 60)
+    .toString()
+    .padStart(2, "0")
+  seconds = seconds % 60
+
+  const s = Math.round(seconds).toString().padStart(2, "0")
+
+  const ms = Math.round((seconds - Math.floor(seconds)) * 1000)
+    .toString()
+    .padEnd(3, "0")
+
+  return `${h}:${m}:${s}.${ms}`
+}
