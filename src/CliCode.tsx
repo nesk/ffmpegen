ed8806ebdd642a7542961c632295486669ec605a
Johann Pardanaud <johann@pardanaud.com>
2022-02-21 13:27:12 +0100

    add a button to copy the code

--- src/CliCode.tsx
+++ src/CliCode.tsx
@@ -2,6 +2,8 @@ import { useEffect } from "react"
 import { FC, useState } from "react"
 import styled from "styled-components"
 import { formatSecondsToFfmpegTime } from "./helpers"
+import { ReactComponent as CopyIcon } from "./assets/copy.svg"
+import { ReactComponent as CheckIcon } from "./assets/check.svg"
 
 const Layout = styled.section`
   grid-area: cli;
@@ -10,12 +12,67 @@ const Layout = styled.section`
   line-height: 2;
   font-size: 18px;
   overflow: auto;
+
+  &:not(:hover) {
+    button {
+      visibility: hidden;
+    }
+  }
+
+  button {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
 `
 
 const Code = styled.pre`
   width: min-content;
   padding: 30px;
 `
+
+const Button = styled.button`
+  appearance: none;
+  display: flex;
+  border: 1px solid #272727;
+  border-radius: 6px;
+  padding: 10px;
+  background-color: #1e1e1e;
+  color: #b7b7b7;
+  cursor: pointer;
+
+  &:hover {
+    border-color: currentColor;
+    color: #ffcc01;
+  }
+
+  svg {
+    width: 18px;
+  }
+`
+interface CopyButtonProps {
+  readonly onClick: () => void
+}
+
+const CopyButton: FC<CopyButtonProps> = ({ onClick }) => {
+  const [didCopy, setDidCopy] = useState(false)
+
+  const handleClick = () => {
+    onClick()
+    setDidCopy(true)
+    setTimeout(() => setDidCopy(false), 1500)
+  }
+
+  return (
+    <Button
+      onClick={didCopy ? undefined : handleClick}
+      style={didCopy ? { borderColor: "currentColor", color: "#6aeb1b" } : {}}
+      title="Copy"
+    >
+      {didCopy ? <CheckIcon style={{ transform: "scale(0.9)" }} /> : <CopyIcon />}
+    </Button>
+  )
+}
 
 interface CliCodeProps {
   file?: File | null
@@ -28,6 +85,7 @@ export const CliCode: FC<CliCodeProps> = ({ file, startTime, endTime }) => {
   const [outputName, setOutputName] = useState("<output>")
   const [outputExtension, setOutputExtension] = useState("")
 
+  // Update the output name when the file changes
   useEffect(() => {
     if (!file) return
     const [extension, ...name] = file.name.split(".").reverse()
@@ -35,6 +93,7 @@ export const CliCode: FC<CliCodeProps> = ({ file, startTime, endTime }) => {
     setOutputExtension(extension)
   }, [file])
 
+  // Allow to rename the output file when pressing "R"
   useEffect(() => {
     const handleKeypress = (e: KeyboardEvent) => {
       if (e.code !== "KeyR") return
@@ -46,21 +105,23 @@ export const CliCode: FC<CliCodeProps> = ({ file, startTime, endTime }) => {
     return () => window.removeEventListener("keypress", handleKeypress)
   }, [])
 
+  const cliCode = `
+    ffmpeg \\
+      -i '${input}' \\
+      -ss ${formatSecondsToFfmpegTime(startTime ?? 0)} \\
+      -to ${formatSecondsToFfmpegTime(endTime ?? 0)} \\
+      -c copy \\
+      '${outputName}${outputExtension && `.${outputExtension}`}'
+  `
+    .trim() // trim all the spaces around the code
+    .replace(/ {2,}/g, "  ") // change the indentation for 2 spaces only
+
+  const copyCliCode = () => navigator.clipboard.writeText(cliCode)
+
   return (
     <Layout>
-      <Code>
-        ffmpeg \
-        <br />
-        {"  "}-i '{input}' \
-        <br />
-        {"  "}-ss {formatSecondsToFfmpegTime(startTime ?? 0)} \
-        <br />
-        {"  "}-to {formatSecondsToFfmpegTime(endTime ?? 0)} \<br />
-        {"  "}-c copy \
-        <br />
-        {"  "}'{outputName}
-        {outputExtension && `.${outputExtension}`}'
-      </Code>
+      <CopyButton onClick={copyCliCode} />
+      <Code dangerouslySetInnerHTML={{ __html: cliCode }} />
     </Layout>
   )
 }
